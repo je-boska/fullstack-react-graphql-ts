deca3b69badb40ddec4604466ecf466566e92fe3
joneirikboska <joneirikboska@gmail.com>
2020-12-15 09:22:53 +0100

    Set up mikroOrm config. Install server packages.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
+import { Post } from './entities/Post'
+import microConfig from './mikro-orm.config'
+import express from 'express'
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    dbName: 'lireddit',
-    type: 'postgresql',
-    debug: !__prod__,
-    // USERNAME and PASSWORD here if necessary
+  // CONNECT TO DATABASE
+  const orm = await MikroORM.init(microConfig)
+
+  // RUN MIGRATIONS
+  await orm.getMigrator().up()
+
+  const app = express()
+  app.listen(4000, () => {
+    console.log('Server started on localhost:4000')
   })
 }
 
-main()
+main().catch(err => {
+  console.error(err)
+})
