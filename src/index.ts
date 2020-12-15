260c1a194b037295e39373c5bafd888bab641fde
joneirikboska <joneirikboska@gmail.com>
2020-12-15 09:37:52 +0100

    Create server with graphql endpoint.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
-import { Post } from './entities/Post'
 import microConfig from './mikro-orm.config'
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
 
 const main = async () => {
   // CONNECT TO DATABASE
@@ -12,6 +14,17 @@ const main = async () => {
   await orm.getMigrator().up()
 
   const app = express()
+
+  // CREATE GRAPHQL ENDPOINT ON SERVER
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  })
+
+  apolloServer.applyMiddleware({ app })
+
   app.listen(4000, () => {
     console.log('Server started on localhost:4000')
   })
