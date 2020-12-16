28c402e5cfcb0ba5007a141d59bfb353bd55452a
joneirikboska <joneirikboska@gmail.com>
2020-12-16 12:13:08 +0100

    Create user entity and resolvers, error handling.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { HelloResolver } from './resolvers/hello'
 import { PostResolver } from './resolvers/post'
+import { UserResolver } from './resolvers/user'
 
 const main = async () => {
   // CONNECT TO DATABASE
@@ -20,7 +21,7 @@ const main = async () => {
   // CREATE GRAPHQL ENDPOINT ON SERVER
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
