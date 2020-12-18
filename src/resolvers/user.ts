02ebd611d9c3b45d05035dc04487d53401a673cd
joneirikboska <joneirikboska@gmail.com>
2020-12-18 11:16:18 +0100

    Make register user query with querybuilder instead of mikroorm.

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
   Query,
 } from 'type-graphql'
 import argon2 from 'argon2'
+import { EntityManager } from '@mikro-orm/postgresql'
 
 @InputType()
 class UsernamePasswordInput {
@@ -77,16 +78,30 @@ export class UserResolver {
       }
     }
     const hashedPassword = await argon2.hash(options.password)
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    })
+    let user
+    // ORIGINAL QUERY WITH mikroORM, works fine
+    // const user = em.create(User, {
+    //   username: options.username,
+    //   password: hashedPassword,
+    // })
     try {
-      await em.persistAndFlush(user)
+      // ORIGINAL QUERY WITH mikroORM, works fine
+      // await em.persistAndFlush(user)
+
+      // BELOW: EXAMPLE: QueryBuilder instead of mikroORM
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning('*')
+      user = result[0]
     } catch (err) {
-      if (err.code === '23505') {
-        // || err.detail.includes("already exists")) {
-        // duplicate username error
+      if (err.code === '23505' || err.detail.includes('already exists')) {
         return {
           errors: [
             {
