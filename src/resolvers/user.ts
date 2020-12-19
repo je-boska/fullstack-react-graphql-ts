46a46f279a1a86a8b0754539c73b7b50c3cb1549
joneirikboska <joneirikboska@gmail.com>
2020-12-19 14:35:58 +0100

    Completely complete forgot password endpoint.

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -37,6 +37,57 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => UserResponse)
+  async changePassword(
+    @Arg('token') token: string,
+    @Arg('newPassword') newPassword: string,
+    @Ctx() { redis, em, req }: MyContext
+  ): Promise<UserResponse> {
+    if (newPassword.length <= 2) {
+      return {
+        errors: [
+          {
+            field: 'newPassword',
+            message: 'length must be 3 or more characters',
+          },
+        ],
+      }
+    }
+
+    const userId = redis.get(FORGET_PASSWORD_PREFIX + token)
+    if (!userId) {
+      return {
+        errors: [
+          {
+            field: 'token',
+            message: 'token expired',
+          },
+        ],
+      }
+    }
+
+    const user = await em.findOne(User, { id: Number(userId) })
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'token',
+            message: 'user no longer exists',
+          },
+        ],
+      }
+    }
+
+    user.password = await argon2.hash(newPassword)
+    await em.persistAndFlush(user)
+
+    // log in user after change password
+    req.session.userId = user.id
+
+    return { user }
+  }
+
   @Mutation(() => Boolean)
   async forgotPassword(
     @Arg('email') email: string,
