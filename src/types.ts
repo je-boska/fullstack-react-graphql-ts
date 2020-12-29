316a998685af1b14dfcf25abf8260cabdaf89ec8
joneirikboska <joneirikboska@gmail.com>
2020-12-29 09:38:25 +0100

    Define session in types.ts instead of middleware.

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,13 @@
+import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core'
 import { Request, Response } from 'express'
-import { Session } from 'express-session'
+import { Session, SessionData } from 'express-session'
 import { Redis } from 'ioredis'
 
 export type MyContext = {
-  req: Request & { session: Session }
-  redis: Redis
+  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+  req: Request & {
+    session: Session & Partial<SessionData> & { userId?: number }
+  }
   res: Response
+  redis: Redis
 }
